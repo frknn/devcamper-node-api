3cef9bce0138270ab60aea9e1fad174ad94b3701
frknn <frknstbs@gmail.com>
2019-10-23 22:04:32 +0300

    created another mongoose pre middleware to generate location fields from given address using node-geocoder

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -107,6 +108,27 @@ const BootcampSchema = new mongoose.Schema({
 // In below middleware, "this" refers to document that will we saved and can reach its fields
 BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true })
+  next();
+});
+
+
+// Geocode and create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  }
+  
+  // Do not save entered addres in DB because we have formatted one
+  this.address = undefined;
+
   next();
 })
 
