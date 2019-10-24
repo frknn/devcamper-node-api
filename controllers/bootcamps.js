208d2b374a201b0dab0fda8f9145e9317fd3aa82
frknn <frknstbs@gmail.com>
2019-10-24 18:42:06 +0300

    added data filtering with query params, selecting and sorting by specific fields

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,40 @@ const geocoder = require('../utils/geocoder');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let reqQuery = { ...req.query };
+
+  // fields that will be removed from query
+  let removedFields = ['select','sort'];
+
+  // remove unwanted fields from query
+  removedFields.forEach(field => delete reqQuery[field]);
+  console.log(reqQuery);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  /* \b is word boundary regex
+    I used it to get a match with the options I gave in.
+    Last 'g' is for global to catch all matches instead of just first match.
+    Catching the keywords and adding $ at the begginig for mongoDB document querying.
+  */
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select fields
+  if (req.query.select) {
+    let selectedFields = req.query.select.replace(/,/g, ' ');
+    query = query.select(selectedFields);
+  }
+
+  // Sort fields
+  let sortFields = req.query.sort ? req.query.sort.replace(/,/g, ' ') : '-createdAt';
+  query = query.sort(sortFields);
+
+
+  const bootcamps = await query;
 
   res.status(200).json({
     success: true,
