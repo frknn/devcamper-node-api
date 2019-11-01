c7b89c72e9ab508ffb326b46fd871576ec6166fc
frknn <frknstbs@gmail.com>
2019-11-01 18:28:14 +0300

    implemented email sending and resetting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 // @desc    Register user
 // @route   POST /api/v1/auth/register
@@ -75,10 +76,59 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({
-    success: true,
-    data: user
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    });
+
+    res.status(200).json({ success: true, data: 'Email sent!', user });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent!', 500));
+  }
+
+  // res.status(200).json({
+  //   success: true,
+  //   data: user
+  // });
+});
+
+// @desc    Reset Password
+// @route   PUT /api/v1/auth/resetpassword/:resetToken
+// @access  Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+
+  // Get hashed token
+  const resetPwToken = crypto.createHash('sha256').update(req.params.resetToken).digest('hex');
+
+  const user = await User.findOne({
+    resetPasswordToken: resetPwToken,
+    resetPasswordExpire: { $gt: Date.now() }
   });
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid token!', 400));
+  }
+
+  // Set new password, when we set a field to undefined, it goes away, won't be saved into db as fields
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined
+  user.resetPasswordExpire = undefined
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
 });
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getCurrentUser, forgotPassword } = require('../controllers/auth');
+const { register, login, getCurrentUser, forgotPassword, resetPassword } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getCurrentUser);
 router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resetToken', resetPassword)
 
 module.exports = router;
