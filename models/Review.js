907250921dfd631390cafba84289ce7dc85ecf74
frknn <frknstbs@gmail.com>
2019-11-02 15:23:20 +0300

    created middleware to calculate average rating fot bootcamp after adding or deleting a review

--- models/Review.js
+++ models/Review.js
@@ -36,4 +36,45 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+/* Static methods can be called directly from the object itself 
+  they don't need an instance,
+  besides they can't be called from instance */
+
+// Static method for calculating average rating
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+
+  // mongodb aggregate doc
+  // https://docs.mongodb.com/manual/reference/method/db.collection.aggregate/
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: "$rating" }
+      }
+    }
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating
+    })
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Call getAverageRating after save
+ReviewSchema.post('save', async function () {
+  await this.constructor.getAverageRating(this.bootcamp)
+});
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', async function () {
+  await this.constructor.getAverageRating(this.bootcamp)
+});
+
 module.exports = mongoose.model('Review', ReviewSchema);
