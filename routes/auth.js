593909aa7884fac6fe4e9382b3c4492475f37b4e
frknn <frknstbs@gmail.com>
2019-11-02 18:40:23 +0300

    implemented logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getCurrentUser, forgotPassword, resetPassword, updateNameAndMail, updatePassword } = require('../controllers/auth');
+const { register, login, getCurrentUser, forgotPassword, resetPassword, updateNameAndMail, updatePassword, logout } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getCurrentUser);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resetToken', resetPassword);
