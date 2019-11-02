7425cf4187dcf02d0c44624634726a10a7834ee6
frknn <frknstbs@gmail.com>
2019-11-02 19:14:35 +0300

    added packages to sanitize data, prevent NoSQL injection, prevent XSS, security header, added rate limiting like 100 for 10 min, enabled CORS, prevent HPP

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db');
 
@@ -28,6 +34,28 @@ app.use(cookieParser());
 
 // File uploading
 app.use(fileupload());
+
+// Sanitize data for preventing NoSQL injection
+app.use(mongoSanitize());
+
+// Set security header
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100
+});
+app.use(limiter);
+
+// Prevent HTTP parameter pollution attacks
+app.use(hpp());
+
+// Enable CORS - cross origin resource sharing
+app.use(cors());
 
 // Set static folder to reach the source directly from url/source.src
 app.use(express.static(path.join(__dirname, 'public')));
